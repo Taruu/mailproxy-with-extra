c52b81a709c7f5006cd7eac7f04c2f7f12c40f20
Pugemon <64846484+Pugemon@users.noreply.github.com>
2023-03-07 00:47:23 +0300

    Reformat code to BLACK codestyle

--- mailproxy.py
+++ mailproxy.py
@@ -8,7 +8,7 @@ from time import sleep
 
 from aiosmtpd.controller import Controller
 
-__version__ = '1.0.2'
+__version__ = "1.0.2"
 
 
 class MailProxyHandler:
@@ -16,23 +16,10 @@ class MailProxyHandler:
         self._host = host
         self._port = port
         auth = auth or {}
-        self._auth_user = auth.get('user')
-        self._auth_password = auth.get('password')
+        self._auth_user = auth.get("user")
+        self._auth_password = auth.get("password")
         self._use_ssl = use_ssl
         self._starttls = starttls
-
-    async def handle_DATA(self, server, session, envelope):
-        try:
-            refused = self._deliver(envelope)
-        except smtplib.SMTPRecipientsRefused as e:
-            logging.info('Got SMTPRecipientsRefused: %s', refused)
-            return "553 Recipients refused {}".format(' '.join(refused.keys()))
-        except smtplib.SMTPResponseException as e:
-            return "{} {}".format(e.smtp_code, e.smtp_error)
-        else:
-            if refused:
-                logging.info('Recipients refused: %s', refused)
-            return '250 OK'
 
     # adapted from https://github.com/aio-libs/aiosmtpd/blob/master/aiosmtpd/handlers.py
     def _deliver(self, envelope):
@@ -50,54 +37,62 @@ class MailProxyHandler:
                 s.login(self._auth_user, self._auth_password)
             try:
                 refused = s.sendmail(
-                    envelope.mail_from,
-                    envelope.rcpt_tos,
-                    envelope.original_content
+                    envelope.mail_from, envelope.rcpt_tos, envelope.original_content
                 )
             finally:
                 s.quit()
         except (OSError, smtplib.SMTPException) as e:
-            logging.exception('got %s', e.__class__)
+            logging.exception("got %s", e.__class__)
             # All recipients were refused. If the exception had an associated
             # error code, use it.  Otherwise, fake it with a SMTP 554 status code.
-            errcode = getattr(e, 'smtp_code', 554)
-            errmsg = getattr(e, 'smtp_error', e.__class__)
+            errcode = getattr(e, "smtp_code", 554)
+            errmsg = getattr(e, "smtp_error", e.__class__)
             raise smtplib.SMTPResponseException(errcode, errmsg.decode())
 
+    async def handle_DATA(self, server, session, envelope):
+        try:
+            refused = self._deliver(envelope)
+        except smtplib.SMTPRecipientsRefused as e:
+            logging.info("Got SMTPRecipientsRefused: %s", refused)
+            return "553 Recipients refused {}".format(" ".join(refused.keys()))
+        except smtplib.SMTPResponseException as e:
+            return "{} {}".format(e.smtp_code, e.smtp_error)
+        else:
+            if refused:
+                logging.info("Recipients refused: %s", refused)
+            return "250 OK"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     if len(sys.argv) == 2:
         config_path = sys.argv[1]
     else:
-        config_path = os.path.join(
-            sys.path[0],
-            'config.ini'
-        )
+        config_path = os.path.join(sys.path[0], "config.ini")
     if not os.path.exists(config_path):
         raise Exception("Config file not found: {}".format(config_path))
 
     config = configparser.ConfigParser()
     config.read(config_path)
 
-    use_auth = config.getboolean('remote', 'smtp_auth', fallback=False)
+    use_auth = config.getboolean("remote", "smtp_auth", fallback=False)
     if use_auth:
         auth = {
-            'user': config.get('remote', 'smtp_auth_user'),
-            'password': config.get('remote', 'smtp_auth_password')
+            "user": config.get("remote", "smtp_auth_user"),
+            "password": config.get("remote", "smtp_auth_password"),
         }
     else:
         auth = None
 
     controller = Controller(
         MailProxyHandler(
-            host=config.get('remote', 'host'),
-            port=config.getint('remote', 'port', fallback=25),
+            host=config.get("remote", "host"),
+            port=config.getint("remote", "port", fallback=25),
             auth=auth,
-            use_ssl=config.getboolean('remote', 'use_ssl', fallback=False),
-            starttls=config.getboolean('remote', 'starttls', fallback=False),
+            use_ssl=config.getboolean("remote", "use_ssl", fallback=False),
+            starttls=config.getboolean("remote", "starttls", fallback=False),
         ),
-        hostname=config.get('local', 'host', fallback='127.0.0.1'),
-        port=config.getint('local', 'port', fallback=25)
+        hostname=config.get("local", "host", fallback="127.0.0.1"),
+        port=config.getint("local", "port", fallback=25),
     )
     controller.start()
     while controller.loop.is_running():

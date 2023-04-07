fb00a23b034f18750649ef522a0b150ae7b93bd8
Pugemon <64846484+Pugemon@users.noreply.github.com>
2023-04-07 15:31:14 +0000

    REWRITE!

--- mailproxy.py
+++ mailproxy.py
@@ -5,85 +5,93 @@ import os
 import smtplib
 import sys
 import time
+from pathlib import Path
+from typing import Dict, Union
 
 from aiosmtpd.controller import Controller
 from aiosmtpd.smtp import SMTP as SMTPServer
 
 import puglogger
 
-logFile = "pugmailproxy.log"
-logSize = 1000000
-logCount = 10
-logLevel = logging.INFO
+logging_config: Dict[str, Union[str, int, bool]] = {
+    "log_file": "pugmailproxy.log",
+    "log_size": 1000000,
+    "log_count": 10,
+    "log_level": logging.INFO,
+}
 
 
 class UTF8Controller(Controller):
-    def factory(self):
+    def factory(self) -> SMTPServer:
         return SMTPServer(self.handler, decode_data=True, enable_SMTPUTF8=True)
 
 
 class MailProxyHandler:
-    def __init__(self, host, port=0, auth=None, use_ssl=False, starttls=False):
+    def __init__(self, host: str, port: int = 0, auth: dict[str, str] | None = None, use_ssl: bool = False, starttls: bool = False) -> None:
         self._host = host
         self._port = port
         auth = auth or {}
-        self._auth_user = auth.get("user")
-        self._auth_password = auth.get("password")
+        self._auth_user = auth.get("user") if auth else None
+        self._auth_password = auth.get("password") if auth else None
         self._use_ssl = use_ssl
         self._starttls = starttls
 
-    def _deliver(self, envelope):
+    async def _deliver(self, envelope) -> dict[str, str]:
         refused = {}
         try:
-            if self._use_ssl:
-                s = smtplib.SMTP_SSL(self._host, self._port)
-            else:
-                s = smtplib.SMTP(self._host, self._port)
-            if self._starttls:
-                s.starttls()
-                s.ehlo()
-            if self._auth_user and self._auth_password:
-                s.login(self._auth_user, self._auth_password)
-            try:
-                refused = s.sendmail(
-                    envelope.mail_from, envelope.rcpt_tos, envelope.original_content
-                )
-            finally:
-                s.quit()
+            with smtplib.SMTP(self._host, self._port) as s:
+                if self._use_ssl:
+                    s.starttls()
+
+                if self._auth_user and self._auth_password:
+                    s.login(self._auth_user, self._auth_password)
+
+                refused = await asyncio.to_thread(s.sendmail, envelope.mail_from, envelope.rcpt_tos, envelope.original_content)
+
         except (OSError, smtplib.SMTPException) as e:
-            puglog.logerr("got %s", e.__class__)
+            puglog.logerr(f"got {e.__class__}")
             # All recipients were refused. If the exception had an associated
             # error code, use it.  Otherwise, fake it with a SMTP 554 status code.
-            errcode = getattr(e, "smtp_code", 554)
-            errmsg = getattr(e, "smtp_error", e.__class__.__name__.encode())
-            raise smtplib.SMTPResponseException(errcode, errmsg.decode())
+            smtperrcode = getattr(e, "smtp_code", 554)
+            smtperrmsg = getattr(e, "smtp_error", e.__class__.__name__.encode())
+            errormsg = f"{smtperrcode} {smtperrmsg.decode()}"
+            raise smtplib.SMTPResponseException(smtperrcode, errormsg)
+        
+        except Exception as e:
+            puglog.logerr(f"Unexpected error occurred during sendmail: {type(e).__name__}: {e}")
+
+
+        return refused
 
     async def handle_DATA(self, server, session, envelope):
+        refused = {}
         try:
-            refused = self._deliver(envelope)
+            refused = await self._deliver(envelope)
         except smtplib.SMTPRecipientsRefused as e:
-            puglog.log("Got SMTPRecipientsRefused: %s", refused)
+            puglog.log(f"Got SMTPRecipientsRefused: {refused}")
             return "553 Recipients refused {}".format(" ".join(refused.keys()))
         except smtplib.SMTPResponseException as e:
-            return "{} {}".format(e.smtp_code, e.smtp_error)
+            return f"{e.smtp_code} {e.smtp_error}"
         else:
             if refused:
-                puglog.log("Recipients refused: %s", refused)
+                puglog.log(f"Recipients refused: {refused}")
             return "250 OK"
 
 
 if __name__ == "__main__":
-
     puglog = puglogger.Logging(
-        logFile=logFile, logSize=logSize, logCount=logCount, logLevel=logCount
+        logFile=logging_config["log_file"],
+        logSize=logging_config["log_size"],
+        logCount=logging_config["log_count"],
+        logLevel=logging_config["log_level"],
     )
 
     if len(sys.argv) == 2:
         config_path = sys.argv[1]
     else:
-        config_path = os.path.join(sys.path[0], "config.ini")
-    if not os.path.exists(config_path):
-        raise Exception("Config file not found: {}".format(config_path))
+        config_path = Path(sys.path[0]) / "config.ini"
+    if not config_path.exists:
+        raise Exception(f"Config file not found: {config_path}")
 
     config = configparser.ConfigParser()
     config.read(config_path)
@@ -97,31 +105,28 @@ if __name__ == "__main__":
     else:
         auth = None
 
-    controller = UTF8Controller(
-        MailProxyHandler(
-            host=config.get("remote", "host"),
-            port=config.getint("remote", "port", fallback=25),
-            auth=auth,
-            use_ssl=config.getboolean("remote", "use_ssl", fallback=False),
-            starttls=config.getboolean("remote", "starttls", fallback=False),
-        ),
-        hostname=config.get("local", "host", fallback="127.0.0.1"),
-        port=config.getint("local", "port", fallback=25),
+controller = UTF8Controller(
+    MailProxyHandler(
+        host=config.get("remote", "host"),
+        port=config.getint("remote", "port", fallback=25),
+        auth=auth,
+        use_ssl=config.getboolean("remote", "use_ssl", fallback=False),
+        starttls=config.getboolean("remote", "starttls", fallback=False),
+    ),
+    hostname=config.get("local", "host", fallback="127.0.0.1"),
+    port=config.getint("local", "port", fallback=25),
+)
+try:
+    controller.start()
+    puglog.log(
+        "Mail proxy starting on port:{}".format(config.getint("local", "port"))
     )
-    try:
-        controller.start()
-        puglog.log(
-            "Mail proxy starting on port:{}".format(config.getint("local", "port"))
-        )
-        while controller.loop.is_running():
-            time.sleep(1)
-    except KeyboardInterrupt:
-        controller.stop()
-        puglog.logwarn("Mail proxy stopped by user")
-    except:
-        puglog.logerr(
-            "Caught unknown exception: "
-            + str(sys.exc_info()[0])
-            + ": "
-            + str(sys.exc_info()[1])
-        )
+    while controller.loop.is_running():
+        asyncio.sleep(1)
+except (KeyboardInterrupt, asyncio.CancelledError):
+    controller.stop()
+    if hasattr(controller, '_cleanup'):
+        controller._cleanup()
+    puglog.logwarn("Mail proxy stopped by user")
+except Exception as e:
+    puglog.logerr(f"Caught unknown exception: {type(e).__name__}: {e}")

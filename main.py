cdedce5e824e96196a64c733e9babe44af0dfae1
Pugemon <pugemon03@gmail.com>
2023-04-14 18:24:02 +0300

    Refactor code

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 from pathlib import Path
 from time import sleep
 import logging
+from typing import Dict, List
 
 from user_handlers import SmtpHandler, ImapHandler, MailUser
 from aiosmtpd.controller import Controller
@@ -14,26 +15,33 @@ class LocalSmtpHandler:
     """Class for handling SMTP requests from local server"""
 
     def __init__(self):
-        self.mail_users = {}
+        self.mail_users: Dict[str, MailUser] = {}  # dict of users with username and handler for each user.
 
-    def load_users(self, config: configparser.ConfigParser):
+    def load_users(self, config: configparser.ConfigParser) -> None:
         """Loads users from config file
 
         Args:
             config (configparser.ConfigParser): Config file
 
         """
-        list_emails = config.get("local", "email_list").split(",")
-        loaded_users = {}
+        try:
+            list_emails = config.get("local", "email_list").split(",")
+        except (configparser.NoOptionError, configparser.NoSectionError) as e:
+            raise ValueError(f"Error reading config file: {e}") from e
+
+        loaded_users: Dict[str, MailUser] = {}
         for email in list_emails:
-            smtp_handler = SmtpHandler.load_smtp(config, email)
-            imap_handler = ImapHandler.load_imap(config, email)
-            temp_mail_user = MailUser(email, smtp_handler, imap_handler)
-            loaded_users[email] = temp_mail_user
+            try:
+                smtp_handler = SmtpHandler.load_smtp(config, email)
+                imap_handler = ImapHandler.load_imap(config, email)
+                temp_mail_user = MailUser(email, smtp_handler, imap_handler)
+                loaded_users[email] = temp_mail_user
+            except Exception as e:
+                raise ValueError(f"Error while loading user: {e}") from e
 
         self.mail_users = loaded_users
 
-    async def handle_data(self, server, session, envelope):
+    async def handle_data(self, server, session, envelope) -> str:
         """Handle DATA command from SMTP server.
 
         Args:
@@ -46,13 +54,17 @@ class LocalSmtpHandler:
 
         """
 
-        email_from = envelope.mail_from
-        emails_to = envelope.rcpt_tos
-        content = envelope.original_content
-        refused_recipients = {}
+        email_from: str = envelope.mail_from
+        emails_to: List[str] = envelope.rcpt_tos
+        content: bytes = envelope.original_content
+        refused_recipients: Dict[str, str] = {}
 
         try:
-            mail_user = self.mail_users.get(email_from)
+            mail_user = self.mail_users[email_from]
+        except KeyError:
+            logging.error(f"Error: From {email_from} not in config file")            # noqa: E501 # pylint: disable=line-too-long # noqa: E501 # p
+            return "550 User not found"
+        try:
             mail_user.smtp_handler.implement_email(emails_to, content)
             mail_user.imap_handler.implement_email(emails_to, content)
 
@@ -63,12 +75,17 @@ class LocalSmtpHandler:
         except aiosmtpd.SMTPResponseException as e:
             logging.error(f"SMTP response exception: {e.smtp_code} {e.smtp_error}")
             return f"{e.smtp_code} {e.smtp_error}"
+        """ else:
+            logging.info("250 Message accepted for delivery")
+            return "250 OK" """
+            
+        if refused_recipients:
+            return f"553 Recipients refused: {refused_recipients}"
         else:
-            logging.info(f"Successfully handled DATA command")
-            return "250 OK"
+            return "250 Message accepted for delivery"
 
 
-# TODO Test it and make it work with real server and config file.
+# TODO: Test it and make it work with real server and config file.
 class UTF8Controller(Controller):
     """Allow UTF8 in SMTP server"""
 
@@ -85,8 +102,9 @@ if __name__ == "__main__":
     if not Path(config_path).exists():
         raise OSError(f"Config file not found: {config_path}")
 
+with open(config_path, "r") as f: #Use context manager  to automatically close the file after reading
     config = configparser.ConfigParser()
-    config.read(config_path)
+    config.read_file(f)
 
     local_handler = LocalSmtpHandler()
     local_handler.load_users(config)
